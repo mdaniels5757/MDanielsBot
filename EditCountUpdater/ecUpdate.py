18fd5d9d749b49f5fe74c8722782b7ecfed0df1a
Michael Daniels <mdaniels5757@gmail.com>
2020-05-29 22:22:35 -0400

    Support commonswiki

--- EditCountUpdater/ecUpdate.py
+++ EditCountUpdater/ecUpdate.py
@@ -42,3 +42,33 @@ for userpage in enGlobalTemplatePage.embeddedin(namespaces=2):
     subpage = pywikibot.Page(enwiki, subpage_name)
     if subpage.text != globalEC:
         subpage.put(globalEC, summary="Updating edit count")
+
+commonswiki = pywikibot.Site('commons', 'commons');
+commonsLocalTemplatePage = pywikibot.Page(enwiki, 'User:MDanielsBot/LocalEC')
+commonsGlobalTemplatePage = pywikibot.Page(enwiki, 'User:MDanielsBot/GlobalEC')
+
+# Local EC for commonswiki
+for userpage in commonsLocalTemplatePage.embeddedin(namespaces=2):
+    if userpage.depth != 0: continue;
+    username = userpage.title(with_ns=False)
+    user = pywikibot.page.User(commonswiki, userpage.title(with_ns=False));
+    if (user.groups() == ['*', 'user'] or user.groups() == ['*', 'user', 'autoconfirmed']):
+        continue;
+    localEC = user.editCount();
+    subpage_name = commonsLocalTemplatePage.title() + '/' + username
+    subpage = pywikibot.Page(commonswiki, subpage_name)
+    if subpage.text != localEC:
+        subpage.put(localEC, summary="Updating edit count")
+    
+# Global EC for commonswiki
+for userpage in commonsGlobalTemplatePage.embeddedin(namespaces=2):
+    if userpage.depth != 0: continue;
+    username = userpage.title(with_ns=False)
+    user = pywikibot.page.User(commonswiki, userpage.title(with_ns=False));
+    if (user.groups() == ['*', 'user'] or user.groups() == ['*', 'user', 'autoconfirmed']):
+        continue;
+    globalEC = globaleditcount(user);
+    subpage_name = commonsGlobalTemplatePage.title() + '/' + username
+    subpage = pywikibot.Page(commonswiki, subpage_name)
+    if subpage.text != globalEC:
+        subpage.put(globalEC, summary="Updating edit count")
